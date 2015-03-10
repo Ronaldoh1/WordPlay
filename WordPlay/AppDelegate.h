bedb0ba9152f6fcf851eee5b317fecd27c331167
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-10 14:55:54 -0700

    Initial Commit

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Ronald Hernandez on 3/10/15.
+//  Copyright (c) 2015 Ron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
