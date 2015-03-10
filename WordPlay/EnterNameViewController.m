b7a67aa7745ead8021e9fa163cd31d8841196311
Timothy Yeh <timothy.yeh@gmail.com>
2015-03-10 15:38:01 -0700

    finalizing step 6

--- WordPlay/EnterNameViewController.m
+++ WordPlay/EnterNameViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "EnterNameViewController.h"
+#import "EnterAdjectiveViewController.h"
 
 @interface EnterNameViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *nameTextField;
@@ -20,19 +21,15 @@
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(UIButton *)sender
+{
+    EnterAdjectiveViewController *viewController = segue.destinationViewController;
+    
+    viewController.title = self.nameTextField.text;
+    
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
