e796f1b901bdbd8aadcbb97d8aa58bb2591977a6
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-10 21:29:01 -0700

    bolded letters

--- WordPlay/ResultsViewController.m
+++ WordPlay/ResultsViewController.m
@@ -18,7 +18,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.resultsTextView.text = [NSString stringWithFormat:@"%@ likes to wear %@ pants and likes to %@ %@",self.name, self.adjective, self.verb, self.adverb];
+
+
+
+
+
+    NSString *someString = [NSString stringWithFormat:@"%@ likes to wear %@ pants and likes to %@ %@",self.name, self.adjective, self.verb, self.adverb];
+
+    NSString *firstString = [NSString stringWithFormat:@"%@", self.name];
+    //myLabel.text=[NSString stringWithFormat:@"%@: 2012/10/14 %@",@"Updated:",@"21:59 PM"];
+
+
+
+    NSRange range1 = [someString rangeOfString:firstString];
+   // NSRange range2 = [myLabel.text rangeOfString:@"21:59 PM"];
+    NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] initWithString:someString];
+
+    [attributedText setAttributes:@{NSFontAttributeName:[UIFont boldSystemFontOfSize:17.0f]} range:range1];
+    //[attributedText setAttributes:@{NSFontAttributeName:[UIFont boldSystemFontOfSize:17.0f]} range:range2];
+
+
+
+    self.resultsTextView.attributedText = attributedText;
+
+
 
 }
 
